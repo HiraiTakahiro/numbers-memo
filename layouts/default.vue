7789ee140f78bd983189b7d1e1289e2a24420bdc
hirai <thirai.0224@gmail.com>
2021-10-16 19:15:52 +0900

    コード移植

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app class="red accent-3 white--text">
+      <v-toolbar-title v-text="'Numbers Memo'" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
