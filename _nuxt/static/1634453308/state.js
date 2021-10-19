3687b128795747756c8fb99ef7922883ec814a34
hirai <thirai.0224@gmail.com>
2021-10-19 20:38:55 +0900

    10月19日deploy

--- _nuxt/static/1634453308/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnumbers-memo\u002F_nuxt\u002Fstatic\u002F1634453308",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnumbers-memo\u002F",assetsPath:"\u002Fnumbers-memo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
